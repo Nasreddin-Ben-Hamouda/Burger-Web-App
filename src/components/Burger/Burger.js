e1c9e867c602bc7d8422d0a9fe050d1734fd79a1
Nasr Eddine <nasreddinbenhamouda@gmail.com>
2020-12-24 18:00:03 +0100

    all necessary burger components (stateless & stateful)

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,8 @@
+import React from "react"
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
+const burger=()=>{
+    return(
+        <BurgerIngredient type="bread-bottom"/>
+    )
+}
+export default burger;
