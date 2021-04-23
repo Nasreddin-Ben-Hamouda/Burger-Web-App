3e9048391270246777abfcd66edcfd67c19748b1
Nasr Eddine <nasreddinbenhamouda@gmail.com>
2021-04-23 07:08:50 +0100

    working with redux saga for handling  all side effect and asynchronous code

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from "redux-saga";
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import { watchAuth, watchBurgerBuilder, watchOrder } from "./store/sagas";
 
 const composeEnhancers = process.env.NODE_ENV==="development"?window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ :null || compose;
 
@@ -19,9 +21,15 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk,sagaMiddleware)
 ));
+sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchBurgerBuilder);
+sagaMiddleware.run(watchOrder);
+
 ReactDOM.render(
 
   <React.StrictMode>
